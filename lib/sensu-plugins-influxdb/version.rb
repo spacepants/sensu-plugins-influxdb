0f9be488c8fe1a2058e28b28fd223752dfa6c9e1
Paul Bailey <paulba@stedwards.edu>
2017-07-22 06:10:58 -0500

    internal release prep

--- lib/sensu-plugins-influxdb/version.rb
+++ lib/sensu-plugins-influxdb/version.rb
@@ -3,7 +3,8 @@ module SensuPluginsInfluxdb
     MAJOR = 1
     MINOR = 2
     PATCH = 0
+    INTERNAL = 'seu'
 
-    VER_STRING = [MAJOR, MINOR, PATCH].compact.join('.')
+    VER_STRING = [MAJOR, MINOR, PATCH, INTERNAL].compact.join('.')
   end
 end
